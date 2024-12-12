2e514a87e744216215452336aa92ce7e30953cca
Eder Queiroz <edix122.edi@hotmail.com>
2024-12-11 21:41:55 -0300

    iniciar busca de perticipantes

--- src/main/java/web/eventos361/repository/ParticipanteRepository.java
+++ src/main/java/web/eventos361/repository/ParticipanteRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import web.eventos361.model.Evento;
 import web.eventos361.model.Participante;
 import web.eventos361.model.Usuario;
+import web.eventos361.repository.queries.participante.ParticipanteQueries;
 
-public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
+public interface ParticipanteRepository extends JpaRepository<Participante, Long>, ParticipanteQueries {
     Participante findByUsuarioAndEvento(Usuario usuario, Evento evento);
 }
---
+++ src/main/java/web/eventos361/repository/queries/participante/ParticipanteQueries.java
@@ -0,0 +1,11 @@
+package web.eventos361.repository.queries.participante;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import web.eventos361.model.Participante;
+
+public interface ParticipanteQueries {
+
+    Page<Participante> pesquisar(Long idEvento, Pageable pageable);
+
+}
